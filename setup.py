985f1e1fa6439c15938451107176e233ad676492
Atchaya T <atchaya.602@gmail.com>
2025-11-17 15:37:08 +0530

    my third commit

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='firstdevops_demo',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=
-        ['flask','pytest'],
+    install_requires=[
+        'flask',
+        'pytest'
+        ],
 )
