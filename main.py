f4f0ef96bef03386ce57b6cc15f5ead9d11cbaa3
Atchaya T <atchaya.602@gmail.com>
2025-11-18 18:09:39 +0530

    my third commit

--- main.py
+++ main.py
@@ -32,4 +32,4 @@ def add_user():
     return jsonify(new_user), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
---
+++ pytest_demo/test_math.py
@@ -0,0 +1,13 @@
+def inc(x):
+    return x + 1
+
+
+def test_method1():
+    assert inc(4) == 5
+    
+    
+def test_method2():
+    assert inc(30) == 31
+    
+def test_method3():
+    assert inc(45) == 46
